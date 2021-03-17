41a3284a602461a2a64f4fe23e7af8e20b9df92f
Siong Chin <schin@ccdc.cam.ac.uk>
2021-03-17 18:58:50 +0000

    Handle when there's no file

--- main.py
+++ main.py
@@ -22,17 +22,19 @@ if __name__ == '__main__':
     files = os.environ.get('INPUT_FILES', '')
     new_files = bool(int(os.environ.get('INPUT_NEW_FILES', 0)))
     print(files)
+    print(files.split(','))
     print(new_files)
 
     retval = 0
 
-    for filepath in files.split(','):
-        githooks.trim_trailing_whitespace_in_file(filepath, new_files)
+    if files:
+        for filepath in files.split(','):
+            githooks.trim_trailing_whitespace_in_file(filepath, new_files)
 
-        retval += githooks.check_do_not_merge_in_file(filepath, new_files)
-        retval += githooks.check_filename(filepath)
-        data = githooks.get_file_content(filepath)
-        if data is not None:
-            retval += githooks.check_file_content(filepath, data)
+            retval += githooks.check_do_not_merge_in_file(filepath, new_files)
+            retval += githooks.check_filename(filepath)
+            data = githooks.get_file_content(filepath)
+            if data is not None:
+                retval += githooks.check_file_content(filepath, data)
 
     sys.exit(retval)
