6cdc4aa1ebc9353a43c83cd19528e6fa2f66c186
Siong Chin <schin@ccdc.cam.ac.uk>
2021-03-17 17:41:40 +0000

    Action to accept inputs (files, is_new_files)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ This is a github action entry point.
 
 '''
 
+import os
 import sys
 
 sys.path.insert(0, './main')
@@ -11,20 +12,25 @@ import githooks
 
 if __name__ == '__main__':
 
-    #import os
-    #for k,v in os.environ.items():
-    #    print(f'{k}: {v}')
+    for k,v in os.environ.items():
+        print(f'{k}: {v}')
     print(f'Checking {githooks.get_event()} by {githooks.get_user()}'
           f'in {githooks.get_branch()}')
 
-    filepath = sys.argv[1]
-    githooks.trim_trailing_whitespace_in_file(filepath, True)
+    files = os.environ.get('INPUT_FILES', [])
+    new_files = bool(os.environ.get('INPUT_NEW_FILES', False))
+    print(files)
+    print(new_files)
 
     retval = 0
-    retval += githooks.check_do_not_merge_in_file(filepath, True)
-    retval += githooks.check_filename(filepath)
-    data = githooks.get_file_content(filepath)
-    if data is not None:
-        retval += githooks.check_file_content(filepath, data)
+
+    for filepath in files:
+        githooks.trim_trailing_whitespace_in_file(filepath, new_files)
+
+        retval += githooks.check_do_not_merge_in_file(filepath, new_files)
+        retval += githooks.check_filename(filepath)
+        data = githooks.get_file_content(filepath)
+        if data is not None:
+            retval += githooks.check_file_content(filepath, data)
 
     sys.exit(retval)
