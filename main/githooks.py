8150e751aa5f50b153c08d37a6e22f5ab9cff824
Siong Chin <schin@ccdc.cam.ac.uk>
2021-03-23 11:51:57 +0000

    Or remotes/origin?

--- main/githooks.py
+++ main/githooks.py
@@ -127,8 +127,8 @@ def get_commit_files():
     '''
     if _is_github_event():
         if os.environ['GITHUB_EVENT_NAME'] == 'pull_request':
-            print(f'git diff --name-status refs/heads/{os.environ["GITHUB_BASE_REF"]}..refs/heads/{os.environ["GITHUB_HEAD_REF"]} --')
-            output = _get_output(f'git diff --name-status refs/heads/{os.environ["GITHUB_BASE_REF"]}..refs/heads/{os.environ["GITHUB_HEAD_REF"]} --')
+            print(f'git diff --name-status remotes/origin/{os.environ["GITHUB_BASE_REF"]}..remotes/origin/{os.environ["GITHUB_HEAD_REF"]} --')
+            output = _get_output(f'git diff --name-status remotes/origin/{os.environ["GITHUB_BASE_REF"]}..remotes/origin/{os.environ["GITHUB_HEAD_REF"]} --')
         else:
             output = _get_output('git diff --name-status HEAD~.. --')
     else:
