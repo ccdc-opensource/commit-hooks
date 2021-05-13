7f1680477dba80a47fe05773384dc9bb7fc93321
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-05-13 19:25:31 +0100

    Add a main csharp file BLD-5717

---
+++ src/CommitHooksMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ccdc.CommitHooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
